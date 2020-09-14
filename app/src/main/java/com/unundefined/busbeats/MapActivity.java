bcb6f6d463bc8a6338de10552c002740f320a2b0
Osama <osama.salman99@gmai.com>
2020-09-15 00:27:52 +0300

    update

--- app/src/main/java/com/unundefined/busbeats/MapActivity.java
+++ app/src/main/java/com/unundefined/busbeats/MapActivity.java
@@ -1,8 +1,8 @@
 package com.unundefined.busbeats;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
 
