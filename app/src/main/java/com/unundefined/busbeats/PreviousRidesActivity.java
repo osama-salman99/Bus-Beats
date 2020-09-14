47c38e231c8e67e6b1ba49d664cf82735ba4b64b
Osama <osama.salman99@gmai.com>
2020-09-14 17:28:27 +0300

    update

---
+++ app/src/main/java/com/unundefined/busbeats/PreviousRidesActivity.java
@@ -0,0 +1,14 @@
+package com.unundefined.busbeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviousRidesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_rides);
+    }
+}
